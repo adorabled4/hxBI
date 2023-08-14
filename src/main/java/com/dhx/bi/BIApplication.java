6b8398cb304f2a13c3ab930b9d862f35a4deb0cc
adorabled4 <dhx2648466390@163.com>
2023-08-14 20:55:54 +0800

    refactor: 解决项目启动依赖问题

--- src/main/java/com/dhx/bi/BIApplication.java
+++ src/main/java/com/dhx/bi/BIApplication.java
@@ -4,10 +4,12 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 @MapperScan("com.dhx.bi.mapper")
 @EnableScheduling
+@EnableWebMvc
 public class BIApplication {
 
     public static void main(String[] args) {
