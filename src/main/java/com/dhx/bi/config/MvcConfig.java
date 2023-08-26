e347092d1fc8bd1e6477a5c831095243dca4fe2a
adorabled4 <dhx2648466390@163.com>
2023-08-26 13:26:47 +0800

    fix: 修复Knife4j文档无法访问的bug

--- src/main/java/com/dhx/bi/config/MvcConfig.java
+++ src/main/java/com/dhx/bi/config/MvcConfig.java
@@ -2,9 +2,11 @@ package com.dhx.bi.config;
 
 import com.dhx.bi.aop.ReFreshTokenInterceptor;
 import com.dhx.bi.service.JwtTokensService;
+import com.github.xiaoymin.knife4j.spring.annotations.EnableKnife4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.annotation.Resource;
@@ -15,6 +17,7 @@ import javax.annotation.Resource;
  * @date : 2023/01/07/ 14:55
  **/
 @Configuration
+@EnableKnife4j
 public class MvcConfig implements WebMvcConfigurer {
 
 
@@ -29,8 +32,8 @@ public class MvcConfig implements WebMvcConfigurer {
         //注意不要拦截knife4j的接口文档
         registry.addInterceptor(new ReFreshTokenInterceptor(stringRedisTemplate,jwtTokensService)).addPathPatterns("/**")
                 .excludePathPatterns(
-                        "/**/user/login",
-                        "/**/register",
+                        "/**/login/**",
+                        "/**/register/**",
                         "/**/doc.html/**",
                         "/static/**",
                         "/**/swagger-ui.html/**",
@@ -42,4 +45,15 @@ public class MvcConfig implements WebMvcConfigurer {
                 );
         WebMvcConfigurer.super.addInterceptors(registry);
     }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/static/");
+        registry.addResourceHandler("doc.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+
 }
